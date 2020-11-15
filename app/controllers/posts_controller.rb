3821b3e6b488366e2fa5c3bcc873abb843344a29
Daichi Kamiyama <dawt2h@gmail.com>
2020-11-15 23:10:05 +0900

    GETできるよう修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
-module Api
-  module V1
-    class PostsController < ApplicationController
-    end
+class PostsController < ApplicationController
+  def index
+    posts = Post.order(created_at: :desc)
+    render json: { status: 'SUCCESS', message: 'Loaded posts', data: posts }
   end
 end
