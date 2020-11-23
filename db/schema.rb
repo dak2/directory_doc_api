f803e3f874d33074315bb6d28bf2fa07fc082423
dak2 <dawt2h@gmail.com>
2020-11-23 20:43:50 +0900

    Itemを返すエンドポイント追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_23_072540) do
+ActiveRecord::Schema.define(version: 2020_11_23_095839) do
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name"
+    t.integer "price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
 end
